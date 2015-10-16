6afc95b7116e73f13c2b3c6b0f488937b0bdf765
Haniel Martino <haniel_martino@hotmail.com>
2015-10-15 18:38:27 -0700

    Added paper elements for main UI

--- ADictionaryClient/src/com/hantino/adictionaryclient/client/ADictionaryClient.java
+++ ADictionaryClient/src/com/hantino/adictionaryclient/client/ADictionaryClient.java
@@ -2,14 +2,42 @@ package com.hantino.adictionaryclient.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.vaadin.polymer.paper.widget.PaperButton;
+import com.vaadin.polymer.Polymer;
+import com.vaadin.polymer.elemental.Function;
+import com.vaadin.polymer.iron.element.IronIconElement;
+import com.vaadin.polymer.paper.element.PaperDrawerPanelElement;
+import com.vaadin.polymer.paper.element.PaperFabElement;
+import com.vaadin.polymer.paper.element.PaperHeaderPanelElement;
+import com.vaadin.polymer.paper.element.PaperIconItemElement;
+import com.vaadin.polymer.paper.element.PaperRippleElement;
+import com.vaadin.polymer.paper.element.PaperToolbarElement;
+
+import java.util.Arrays;
 
 
 public class ADictionaryClient implements EntryPoint {
+	
   public void onModuleLoad() {
-    // Use Widget API to Create a <paper-button>
-    PaperButton button = new PaperButton("Just a starter!");
-    button.setRaised(true);
-    RootPanel.get().add(button);
+    // Loading icon sets before running application
+	Polymer.importHref(Arrays.asList(
+			"iron-icons/iron-icons.html",
+			PaperIconItemElement.SRC,
+			IronIconElement.SRC,
+			PaperRippleElement.SRC,
+			PaperDrawerPanelElement.SRC,
+			PaperHeaderPanelElement.SRC,
+			PaperToolbarElement.SRC,
+			PaperFabElement.SRC), new Function(){
+		public Object call(Object arg){
+			startApplication();
+			return null;
+		}
+		
+	}); 
   }
+  
+  private void startApplication(){
+	  RootPanel.get().add(new Main());
+  }
+  
 }

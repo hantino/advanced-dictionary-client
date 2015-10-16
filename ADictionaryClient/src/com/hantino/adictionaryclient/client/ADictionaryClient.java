d637f983e965861ae4eb49431690afd12ba3034d
Haniel Martino <haniel_martino@hotmail.com>
2015-10-15 19:36:26 -0700

    Added functionality to connect button and corresponding dialog box

--- ADictionaryClient/src/com/hantino/adictionaryclient/client/ADictionaryClient.java
+++ ADictionaryClient/src/com/hantino/adictionaryclient/client/ADictionaryClient.java
@@ -5,10 +5,13 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.vaadin.polymer.Polymer;
 import com.vaadin.polymer.elemental.Function;
 import com.vaadin.polymer.iron.element.IronIconElement;
+import com.vaadin.polymer.paper.element.PaperButtonElement;
+import com.vaadin.polymer.paper.element.PaperDialogElement;
 import com.vaadin.polymer.paper.element.PaperDrawerPanelElement;
 import com.vaadin.polymer.paper.element.PaperFabElement;
 import com.vaadin.polymer.paper.element.PaperHeaderPanelElement;
 import com.vaadin.polymer.paper.element.PaperIconItemElement;
+import com.vaadin.polymer.paper.element.PaperInputElement;
 import com.vaadin.polymer.paper.element.PaperRippleElement;
 import com.vaadin.polymer.paper.element.PaperToolbarElement;
 
@@ -27,7 +30,10 @@ public class ADictionaryClient implements EntryPoint {
 			PaperDrawerPanelElement.SRC,
 			PaperHeaderPanelElement.SRC,
 			PaperToolbarElement.SRC,
-			PaperFabElement.SRC), new Function(){
+			PaperFabElement.SRC,
+			PaperDialogElement.SRC,
+			PaperInputElement.SRC,
+			PaperButtonElement.SRC), new Function(){
 		public Object call(Object arg){
 			startApplication();
 			return null;
--- ADictionaryClient/src/com/hantino/adictionaryclient/client/Main.java
+++ ADictionaryClient/src/com/hantino/adictionaryclient/client/Main.java
@@ -2,16 +2,37 @@ package com.hantino.adictionaryclient.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.vaadin.polymer.elemental.Event;
+import com.vaadin.polymer.elemental.EventListener;
+import com.vaadin.polymer.elemental.HTMLElement;
+import com.vaadin.polymer.paper.element.*;
 
 public class Main extends Composite {
   interface MainUiBinder extends UiBinder<HTMLPanel, Main> {
   }
 
   private static MainUiBinder ourUiBinder = GWT.create(MainUiBinder.class);
+  
+  @UiField PaperDrawerPanelElement drawerPanel;
+  @UiField HTMLElement content;
+
+  @UiField PaperFabElement connectButton;
+  @UiField PaperDialogElement connectHostDialog;
+  @UiField PaperInputElement hostInput;
+  @UiField PaperTextareaElement portInput;
+  @UiField PaperButtonElement confirmConnectButton;
 
   public Main() {
     initWidget(ourUiBinder.createAndBindUi(this));
+    
+    connectButton.addEventListener("click", new EventListener() {
+        @Override
+        public void handleEvent(Event event) {
+            connectHostDialog.open();
+        }
+      });
   }
 }
